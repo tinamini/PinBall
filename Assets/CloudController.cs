18b08ca8017674ef2c4f2c0d954ac36d05465621
tinamini <atsushi.matsui.7865@gmail.com>
2018-06-04 14:47:13 +0900

    Lesson5 PinBall Project Initial-Commit
    PinBallゲームのステージ作成とプレハブまで作成したコミット。

---
+++ Assets/CloudController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 雲を制御する
+/// </summary>
+public class CloudController : MonoBehaviour
+{
+	#region Field
+	/// <summary>
+	/// 最小サイズ
+	/// </summary>
+	private float minimumSize = 1.0f;
+
+	/// <summary>
+	/// 拡大縮小スピード
+	/// </summary>
+	private float magSpeed = 10.0f;
+
+	/// <summary>
+	/// 拡大率
+	/// </summary>
+	private float magnification = 0.07f;
+	#endregion
+
+	#region Method
+	/// <summary>
+	/// Use this for initialization
+	/// </summary>
+	void Start()
+	{
+
+	}
+
+	/// <summary>
+	/// Update is called once per frame
+	/// </summary>
+	void Update()
+	{
+		// 雲の拡大縮小
+		this.transform.localScale = new Vector3(this.minimumSize + Mathf.Sin(Time.time * this.magSpeed) * this.magnification, this.transform.localScale.y, this.minimumSize + Mathf.Sin(Time.time * this.magSpeed) * this.magnification);
+	}
+	#endregion
+}
---
+++ Assets/StarController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 星を制御する
+/// </summary>
+public class StarController : MonoBehaviour
+{
+	#region Field
+	/// <summary>
+	/// 回転速度
+	/// </summary>
+	private float rotSpeed = 1.0f;
+	#endregion
+
+	#region Method
+	/// <summary>
+	/// Use this for initialization
+	/// </summary>
+	void Start()
+	{
+		// 回転開始の角度設定
+		this.transform.Rotate(0, Random.Range(0, 360), 0);
+	}
+
+	/// <summary>
+	/// Update is called once per frame
+	/// </summary>
+	void Update()
+	{
+		this.transform.Rotate(0, this.rotSpeed, 0);
+	}
+	#endregion
+}
